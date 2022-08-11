1eaa768efb936ddb3c64371be7576fa6a2aaaa60
ihossain5 <ismail.hossain5169@gmail.com>
2022-08-11 12:40:49 +0600

    json web token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../model/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET_KEY, { expiresIn: '3d' })
+  }
+
 // login user
 const loginUser = async (req,res)=>{
     res.json('login')
@@ -10,8 +16,11 @@ const signupUser = async (req,res)=>{
 
     try {
       const user = await User.signup(email, password)
+
+        // create a token
+    const token = createToken(user._id)
   
-      res.status(200).json({user})
+      res.status(200).json({token})
     } catch (error) {
       res.status(400).json({error: error.message})
     }
