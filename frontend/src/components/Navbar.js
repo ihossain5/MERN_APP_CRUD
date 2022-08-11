007f169d9746fbb8b8596e01a03c668ecd6b248d
ihossain5 <ismail.hossain5169@gmail.com>
2022-08-11 15:04:11 +0600

    auth middleware added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
-  const {logout} = useLogout()
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
 
-  const handelClick = ()=>{
+  const handelClick = () => {
     logout()
   }
   return (
     <header>
-        <div className="container">
-            <Link to='/'>
-                <h1>Workout Buddy</h1>
-            </Link>
-            <nav>
-              <div>
-                <button onClick={handelClick}>Logout</button>
-              </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
+      <div className="container">
+        <Link to='/'>
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handelClick}>Logout</button>
+            </div>
+          )}
+          {!user && ( 
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
         </nav>
-        </div>
+      </div>
     </header>
   )
 }
