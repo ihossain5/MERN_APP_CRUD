a1f215e880d3192abf838ebab8cb1c3ca4f1a1c3
ihossain5 <ismail.hossain5169@gmail.com>
2022-08-11 15:36:58 +0600

    making authorization request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import WorkoutForm from "./components/WorkoutForm";
+import { useAuthContext } from "./hooks/useAuthContext";
 import { Home } from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
