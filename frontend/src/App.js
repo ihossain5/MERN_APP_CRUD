3f971586a971f3b2fcefeb62f740d164823e7c72
ihossain5 <ismail.hossain5169@gmail.com>
2022-08-11 13:35:57 +0600

    login/signup form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import WorkoutForm from "./components/WorkoutForm";
 import { Home } from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -13,6 +15,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<WorkoutForm />} />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
           </Routes>
         </div>
       </BrowserRouter>
